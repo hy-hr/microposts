d99f1189a9fc47f9fe962f2733ebef52181f8cd7
hr <spc10p@gmail.com>
2020-08-13 10:54:57 +0000

    kadai2

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,10 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key:'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
-
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key:'micropost_id'
+  has_many :favoreds, through: :reverses_of_favorite, source: :user
 
   def follow(other_user)
     unless self == other_user
@@ -30,6 +33,25 @@ class User < ApplicationRecord
 
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def favor(other_micropost)
+    unless self == other_micropost
+      self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+    end
+  end 
+  
+  def unfavor(other_micropost)
+    favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def like?(other_micropost)
+    self.likes.include?(other_micropost)
+  end
+
+  def feed_favorites
+    Micropost.where(user_id: self.like_ids + [self.id])
   end
 
 end
